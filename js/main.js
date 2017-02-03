be033ff35eb06215296522fde656e16e4168a802
Seif <seif.dorani@gmail.com>
2017-02-03 13:45:36 -0500

    event handler, and we are using key up event, the 2nd parameter is a callback function, to test it we log the event and should get a key pressed log in the dom

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-  console.log('ready');
+  $('#searchUser').on('keyup', function(){
+    console.log('keypressed');
+  });
 });
+//connecting
+//we need an event handler on type
