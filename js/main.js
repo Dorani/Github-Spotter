2f5f83577b94c91308d4caf26196d8e0cbbe5da9
Seif <seif.dorani@gmail.com>
2017-02-03 13:54:28 -0500

    makes request to url, sending along our credentials and gives us this user's info in obj format

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
   $('#searchUser').on('keyup', function(e){
     var username = e.target.value;
+
+    // Make request to Github
+    $.ajax({
+      url:'https://api.github.com/users/' + username,
+      data:{
+        client_id:'6f73ca918108541caa59',
+        client_secret:'7b655a83147f4123df2b87cb9edd97d1ff59bde0'
+      }
+    }).done(function(user){
+      console.log(user);
+    });
   });
 });
-//connecting
-//we need an event handler on type
+
+//makes request to url, sending along our credentials and gives us this user's info in obj format
