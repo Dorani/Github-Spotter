5b21f62539876612b2bf851aea29bacf2c413843
Seif <seif.dorani@gmail.com>
2017-02-03 14:04:55 -0500

    bootstrap pannel design to render username in a proper format after ajax request

--- js/main.js
+++ js/main.js
@@ -11,7 +11,14 @@ $(document).ready(function(){
       }
     }).done(function(user){
       $('#profile').html(`
-        ${user.name}
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">${user.name}</h3>
+          </div>
+          <div class="panel-body">
+            Panel content
+          </div>
+        </div>
 
         `);
     });
