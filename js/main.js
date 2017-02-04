03f64065d253c6eb797078a9e7821fca3e042ea7
Seif <seif.dorani@gmail.com>
2017-02-04 00:27:12 -0500

    edits + changes

--- js/main.js
+++ js/main.js
@@ -31,6 +31,7 @@ $(document).ready(function(){
                 <span class="label label-primary">Watchers: ${repo.watchers_count}</span>
                 <span class="label label-success">Stars: ${repo.stargazers_count}</span>
                 <span class="label label-info">Language: ${repo.language}</span>
+                <span class="label label-danger">Contribution: ${repo.contributions}</span>
                 </div>
                 <div class="col-md-2">
                 <a href="${repo.html_url}" targets="_blank" class="btn btn-default">Repo Page</a>
